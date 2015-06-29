d03d437a4b8e2047e93f0f68d5e02195ed9e9dc2
Tamas Kenez <tamas.kenez@gmail.com>
2015-06-29 10:54:54 +0200

    build: add cmake config-module, use cmake 3.1 features
    - raise cmake min to 3.1 (CXX_STANDARD needs it)
    - create meta interface library and install it with config module
    - add explicit option variables
    - debug postfix _d
    - find meta with find_package if not present in build tree
      (needed for build test)
    - add cmake-testbuild.sh to test cmake build and find_package

---
+++ cmake-testbuild.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# - Builds and installs the library in one build tree
+# - Builds and runs the tests in another build tree, finds meta
+#   with find_package so the config module is also tested
+
+set -ex
+
+# install the library in one build tree
+cmake -H. -Bout/build_lib \
+    -DCMAKE_INSTALL_PREFIX=${PWD}/out -DCMAKE_BUILD_TYPE=Release \
+    -DMETA_ENABLE_TEST=0 -DMETA_CUSTOM_CXX_FLAGS=0
+
+cmake --build out/build_lib --target install --config Release
+
+for c in Debug Relase; do
+    # build and run tests in another build tree
+    cmake -H. -Bout/build_test \
+        -DCMAKE_PREFIX_PATH=${PWD}/out -DCMAKE_BUILD_TYPE=$c \
+        -DMETA_CUSTOM_CXX_FLAGS=0 -DMETA_ENABLE_LIBRARY=0
+
+    cmake --build out/build_test --target all --config $c --clean-first
+    cmake --build out/build_test --target RUN_TESTS --config $c \
+        || cmake --build out/build_test --target test --config $c
+done
+
+
